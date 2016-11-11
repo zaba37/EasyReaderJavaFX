738f0387621220652dadfad787bb33dc08fe3c72
Krystian Żabicki <zaba37@MacBook-Air-Apple.local>
2016-11-11 11:19:56 +0100

    Created window for spell checker

---
+++ src/main/java/com/zaba37/easyreader/controllers/SpellCheckerController.java
@@ -0,0 +1,17 @@
+package com.zaba37.easyreader.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by zaba37 on 09.11.2016.
+ */
+public class SpellCheckerController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
